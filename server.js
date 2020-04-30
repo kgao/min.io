17c575a1a8eed3b17ab749c04adefeaa7929bd74
Kevin Gao <kevingao@microsoft.com>
2020-04-29 21:17:51 -0400

    minimal footer style

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 'use strict'
 
 var express = require("express"),
-app = express(),
-router = require("./routes"),
-path = __dirname + '/views/',
-port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000,
-host = process.env.OPENSHIFT_NODEJS_IP;
+  app = express(),
+  router = require("./routes"),
+  path = require('path'),
+  port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000,
+  host = process.env.OPENSHIFT_NODEJS_IP;
 
-app.set('views', path);
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static('./'));
-app.use("/",router);
-app.use('*',function(req,res){
+// serve static content
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/", router);
+app.use('*', function (req, res) {
   res.render('pages/404');
 });
 
-app.listen(port, host, function(){
+app.listen(port, host, function () {
   host = host || 'localhost';
-  console.log('Server is running at '+ host  + ':' + port);
+  console.log('Server is running at ' + host + ':' + port);
 });
